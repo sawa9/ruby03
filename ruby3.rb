b1606e2b542de8446e98c93de7340219c8b3cb0d
sawaki <daigaku68@icloud.com>
2019-11-05 05:33:31 +0000

    保存

---
+++ ruby3.rb
@@ -0,0 +1,32 @@
+n=3
+if n==3
+p 'nは3です'
+end
+n = 4
+if n == 3
+  p 'nは3か4どちらかです'
+elsif n == 4
+  p 'nは3か4どちらかです'
+end
+n = 4
+if n == 3
+  p 'nは3です'
+elsif n == 4
+  p 'nは4です'
+end
+
+for num in 1..5 do
+p "こんにちは"
+end
+
+[1,2,3,4,5].each do |num|
+  p num
+end
+n = 2
+if n == 2
+  p 'nは2です'
+elsif n == 3
+  p 'nは3です'
+else
+  p 'それ以外です'
+end
